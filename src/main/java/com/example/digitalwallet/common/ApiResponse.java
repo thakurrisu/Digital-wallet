83e2ab7843e595df790cdca0bd857777a52be5c2
Rishikesh kumar <rishikesh.kumar.921677@gmail.com>
2026-03-11 00:46:33 +0530

    Added common file : API Reposnse.

---
+++ src/main/java/com/example/digitalwallet/common/ApiResponse.java
@@ -0,0 +1,42 @@
+package com.example.digitalwallet.common;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class ApiResponse<T> {
+
+    private final String message;
+    private final LocalDateTime timestamp;
+    private final boolean success;
+    private final T data;
+
+    //200 - with success and data
+    public ApiResponse<T> ok(String message ,T data){
+       return  ApiResponse.<T>builder()
+               .message(message)
+               .data(data)
+               .success(true)
+               .timestamp(LocalDateTime.now()).build();
+    }
+
+    public ApiResponse<T> ok(String message){
+        return  ApiResponse.<T>builder()
+                .message(message)
+                .data(null)
+                .success(true)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+    public ApiResponse<Void> error(String message){
+        return  ApiResponse.<Void>builder()
+                .message(message)
+                .data(null)
+                .success(false)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+}
