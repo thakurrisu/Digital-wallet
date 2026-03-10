168411c76969e492962589ec4cf00e114c0fd4cd
Rishikesh kumar <rishikesh.kumar.921677@gmail.com>
2026-03-10 21:54:38 +0530

    chore: initial spring boot project setup

---
+++ src/test/java/com/example/digitalwallet/DigitalWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
