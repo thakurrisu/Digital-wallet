6868ffcc7a76dd8c789ab388b8aa8d4ba8085428
Rishikesh kumar <rishikesh.kumar.921677@gmail.com>
2026-03-11 23:33:36 +0530

    Added contracts for user service

--- src/main/java/com/example/digitalwallet/user/service/UserService.java
+++ src/main/java/com/example/digitalwallet/user/service/UserService.java
@@ -1,4 +1,30 @@
 package com.example.digitalwallet.user.service;
 
-public class UserService {
+import com.example.digitalwallet.common.ApiResponse;
+import com.example.digitalwallet.user.dto.*;
+
+import java.util.UUID;
+
+public interface UserService {
+
+    //Register User
+     UserResponse register(RegisterRequest request);
+
+     //Login User
+     AuthResponse login(LoginRequest request);
+
+    //Update User
+    UserResponse updateProfile(UUID id,UpdateRequest request);
+
+    //Update Password
+    UserResponse updatePassword(UUID id,ChangePasswordRequest request);
+
+    //Logout
+    ApiResponse<Void> logout();
+
+    //get user by id
+    UserResponse getUserById(UUID id);
+
+    //delete
+    ApiResponse<Void> delete(UUID id);
 }
