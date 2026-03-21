2e7384ff2604065be3f789268d36d7cf98861102
Rishikesh kumar <rishikesh.kumar.921677@gmail.com>
2026-03-21 16:24:25 +0530

    feat : added Transaction Repo

---
+++ src/main/java/com/example/digitalwallet/transaction/repo/TransactiontRepository.java
@@ -0,0 +1,29 @@
+package com.example.digitalwallet.transaction.repo;
+
+import com.example.digitalwallet.transaction.model.Transaction;
+
+import com.example.digitalwallet.transaction.model.TransactionStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.awt.print.Pageable;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TransactiontRepository extends JpaRepository<Transaction, UUID> {
+
+    Optional<Transaction> findByReferenceId(String referenceId);
+
+    boolean existsByReferenceId(String referenceId);
+
+    Page<Transaction> findByWalletIdOrderByCreatedAtDesc(UUID walletId , Pageable pageable);
+
+    Optional<Transaction> findByIdAndWalletId(UUID id , UUID walletId);
+
+    @Modifying
+    @Query("UPDATE Transaction  t SET t.transactionStatus = :status where t.id= :id")
+    void updateTransactionStatus(@Param("id")  UUID id, @Param("status")TransactionStatus status);
+}
