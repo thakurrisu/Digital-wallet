e91966d5b7ae485891d3cba4c3337525e23984de
Rishikesh kumar <rishikesh.kumar.921677@gmail.com>
2026-04-21 09:29:30 +0530

    feat : Implemented Redis Template

---
+++ src/main/java/com/example/digitalwallet/common/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.example.digitalwallet.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String,String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        //serilisation
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
---
+++ src/main/java/com/example/digitalwallet/common/config/RedisService.java
@@ -0,0 +1,80 @@
+package com.example.digitalwallet.common.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@Slf4j
+public class RedisService {
+
+     private final RedisTemplate<String, String> redisTemplate;
+
+    private static final String BLACKLIST_PREFIX
+            = "blacklist:token:";
+    private static final String IDEMPOTENCY_PREFIX
+            = "idempotency:";
+    private static final String WALLET_LOCK_PREFIX
+            = "wallet:lock:";
+
+    public RedisService(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void blacklistToken(String token , long ttlMilisec) {
+
+        String key = BLACKLIST_PREFIX+token.hashCode();
+        redisTemplate.opsForValue().set(key, token , Duration.ofMillis(ttlMilisec));
+    }
+
+    public boolean isTokenBlacklisted(String token) {
+        String key = BLACKLIST_PREFIX
+                + token.hashCode();
+        return Boolean.TRUE.equals(
+                redisTemplate.hasKey(key));
+    }
+
+    public Boolean setIfAbsent(String refernceId , String value ,  long ttlMilisec) {
+
+        String key = IDEMPOTENCY_PREFIX+refernceId;
+        Boolean result =  redisTemplate.opsForValue().setIfAbsent(key , value,Duration.ofMillis(ttlMilisec));
+        return Boolean.TRUE.equals(result);
+    }
+
+    public Boolean existIdempotencyKey(String referenceId){
+        String key = IDEMPOTENCY_PREFIX+referenceId.hashCode();
+        Boolean result =  redisTemplate.hasKey(key);
+        return Boolean.TRUE.equals(result);
+    }
+
+    public boolean acquireLock(String walletId,
+                               Duration ttl) {
+        String key = WALLET_LOCK_PREFIX + walletId;
+        Boolean acquired = redisTemplate.opsForValue()
+                .setIfAbsent(key, "locked", ttl);
+
+        if (Boolean.TRUE.equals(acquired)) {
+            log.debug("Lock acquired for wallet: {}",
+                    walletId);
+        }
+
+        return Boolean.TRUE.equals(acquired);
+    }
+
+    public void releaseLock(String walletId) {
+        String key = WALLET_LOCK_PREFIX + walletId;
+        redisTemplate.delete(key);
+        log.debug("Lock released for wallet: {}", walletId);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
+    public boolean exists(String key) {
+        return Boolean.TRUE.equals(
+                redisTemplate.hasKey(key));
+    }
+}
